a52d1d535253be6cc70af77b494b757c31bd2600
Lara Namiq <larasnamiq@gmail.com>
2022-08-21 23:36:08 +0100

    Add CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   function handleClick() {
     if (amount <= balance && amount > 0) {
       setBalance(balance - amount);
-      setMessage(`${amount} transferred.`);
+      setMessage(`£${amount} transferred.`);
     } else {
       setMessage(`insufficient funds`);
     }
@@ -18,16 +18,31 @@ function App() {
   const handleChange = (event) => setAmount(event.target.value);
 
   return (
-    <>
-      <p aria-label="balance">£{balance}</p>
-      <input className="border" aria-label="input" onChange={handleChange} />
-      <button type="button" onClick={handleClick}>
+    <div className="flex flex-col justify-center">
+      <h1
+        aria-label="balance"
+        className=" mx-6 my-6 text-center text-3xl font-semibold"
+      >
+        £{balance}
+      </h1>
+      <input
+        className="my-6 mx-auto border border-purple-400"
+        aria-label="input"
+        onChange={handleChange}
+      />
+      <button
+        type="button"
+        onClick={handleClick}
+        className="mx-auto max-w-fit rounded-full bg-purple-400 py-1 px-4 text-white hover:bg-purple-600"
+      >
         {" "}
         Submit{" "}
       </button>
 
-      <p>{message}</p>
-    </>
+      <p className="mx-auto mt-6 max-w-fit rounded-xl border border-purple-300 bg-purple-100 p-2 text-center">
+        {message}
+      </p>
+    </div>
   );
 }
 
