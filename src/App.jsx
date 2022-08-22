d90ef5f0441424dfed19fc7041c23dca96f6f09f
Lara Namiq <larasnamiq@gmail.com>
2022-08-22 19:26:45 +0100

    Refactor form in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,81 @@
 import { useState } from "react";
 import "./App.css";
+import Form from "./components/Form/Form";
+
+const balance = 100;
 
 function App() {
-  const [balance, setBalance] = useState(100);
-  const [amount, setAmount] = useState("");
-  const [message, setMessage] = useState("");
+  const [transferStatus, setTransferStatus] = useState({ amt: 0, msg: "" });
 
-  function handleClick() {
-    if (amount <= balance && amount > 0) {
-      setBalance(balance - amount);
-      setMessage(`£${amount} transferred.`);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const amt = event.target.elements[0].value;
+
+    if (amt > balance) {
+      setTransferStatus({
+        amt: 0,
+        msg: `You can't transfer more than ${balance}`,
+      });
     } else {
-      setMessage(`insufficient funds`);
+      setTransferStatus({
+        amt,
+        msg: "You transferred ",
+      });
     }
-  }
-
-  const handleChange = (event) => setAmount(event.target.value);
+    event.target.reset();
+  };
 
   return (
-    <div className="flex flex-col justify-center">
-      <h1
-        aria-label="balance"
-        className=" mx-6 my-6 text-center text-3xl font-semibold"
-      >
-        £{balance}
-      </h1>
-      <input
-        className="my-6 mx-auto border border-purple-400"
-        aria-label="input"
-        onChange={handleChange}
-      />
-      <button
-        type="button"
-        onClick={handleClick}
-        className="mx-auto max-w-fit rounded-full bg-purple-400 py-1 px-4 text-white hover:bg-purple-600"
-      >
-        {" "}
-        Submit{" "}
-      </button>
-
-      <p className="mx-auto mt-6 max-w-fit rounded-xl border border-purple-300 bg-purple-100 p-2 text-center">
-        {message}
-      </p>
-    </div>
+    <>
+      <Form handleSubmit={handleSubmit} />
+      <div className="m-5 mx-auto flex flex-col items-center">
+        <p>
+          {transferStatus.msg} {transferStatus.amt > 0 && transferStatus.amt}
+        </p>
+        <p>Your balance is now: {balance - transferStatus.amt}</p>
+      </div>
+    </>
   );
 }
 
 export default App;
+//   function handleClick() {
+//     if (amount <= balance && amount > 0) {
+//       setBalance(balance - amount);
+//       setMessage(`£${amount} transferred.`);
+//     } else {
+//       setMessage(`insufficient funds`);
+//     }
+//   }
+
+//   const handleChange = (event) => setAmount(event.target.value);
+
+//   return (
+//     <div className="flex flex-col justify-center">
+//       <h1
+//         aria-label="balance"
+//         className=" mx-6 my-6 text-center text-3xl font-semibold"
+//       >
+//         £{balance}
+//       </h1>
+//       {/* <input
+//         className="my-6 mx-auto border border-purple-400"
+//         aria-label="input"
+//         onChange={handleChange}
+//       />
+//       <button
+//         type="button"
+//         onClick={handleClick}
+//         className="mx-auto max-w-fit rounded-full bg-purple-400 py-1 px-4 text-white hover:bg-purple-600"
+//       >
+//         {" "}
+//         Submit{" "}
+//       </button> */}
+
+//       <p className="mx-auto mt-6 max-w-fit rounded-xl border border-purple-300 bg-purple-100 p-2 text-center">
+//         {setMessage}
+//       </p>
+//     </div>
+//   );
+// }
